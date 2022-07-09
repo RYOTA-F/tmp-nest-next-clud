5a1667574ab71fa96353ba8a56559c328184ffd8
RYOTA-F <ry.fj0304@gmail.com>
2022-07-09 21:17:24 +0900

    [add] 環境構築 + Todos機能

--- nest/src/database/seeds/todo.seed.ts
+++ nest/src/database/seeds/todo.seed.ts
@@ -1,7 +1,32 @@
 export const todos = [
-  { id: 1, name: 'test1', description: 'テスト1です。', isDone: false },
-  { id: 2, name: 'test2', description: 'テスト2です。', isDone: false },
-  { id: 3, name: 'test3', description: 'テスト3です。', isDone: false },
-  { id: 4, name: 'test4', description: 'テスト4です。', isDone: false },
-  { id: 5, name: 'test5', description: 'テスト5です。', isDone: false },
+  {
+    id: 1,
+    name: 'NestJS環境構築',
+    description: 'Docker上でNestJSを駆動させる',
+    isDone: false,
+  },
+  {
+    id: 2,
+    name: 'Next.js環境構築',
+    description: 'Docker上でNext.jsを駆動させる',
+    isDone: false,
+  },
+  {
+    id: 3,
+    name: 'MySQL環境構築',
+    description: 'Docker上でMySQLを駆動させる',
+    isDone: false,
+  },
+  {
+    id: 4,
+    name: 'TypeORM環境構築',
+    description: 'MySQLとTypeORMを連携',
+    isDone: false,
+  },
+  {
+    id: 5,
+    name: 'API連携',
+    description: 'Next.jsからNestJSAPIを連携',
+    isDone: false,
+  },
 ]
